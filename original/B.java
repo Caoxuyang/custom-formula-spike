9ba45b2854825028ff480d7e6195d3c2ff511731
Xuyang Cao <xuycao@microsoft.com>
2025-04-07 12:07:46 +0800

    Apply MSFT formula on B

--- original/B.java
+++ original/B.java
@@ -5,83 +5,72 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-import software.amazon.awssdk.core.waiters.WaiterResponse;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Object;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
-import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
-import software.amazon.awssdk.services.s3.model.CreateMultipartUploadResponse;
-import software.amazon.awssdk.services.s3.model.CompletedMultipartUpload;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.CompletedPart;
-import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
-import software.amazon.awssdk.services.s3.model.UploadPartRequest;
-import software.amazon.awssdk.services.s3.model.CompleteMultipartUploadRequest;
-import software.amazon.awssdk.services.s3.waiters.S3Waiter;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
-
-
-import java.io.IOException;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.options.BlobParallelUploadOptions;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 
 public class UploadObject2 {
 
     public static void main(String[] args) throws IOException {
-          uploadInputStream("ragbagTextFile3.txt");
+        uploadInputStream("ragbagTextFile3.txt");
     }
+
     public static void uploadInputStream(String fileObjectKeyName) throws IOException {
         File initialFile = new File("src/main/webapp/service_provider_pfp_images/abc.txt");
         InputStream targetStream = new FileInputStream(initialFile);
 
-        String bucketName = "testbucket3rag";
-        Region region = Region.EU_NORTH_1;
-        S3Client s3 = S3Client.builder()
-                .region(region)
-                .build();
+        String containerName = "testbucket3rag";
+        String endpoint = "https://yourstorageaccount.blob.core.windows.net";
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .endpoint(endpoint)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+        BlobClient blobClient = containerClient.getBlobClient(fileObjectKeyName);
+
         Map<String, String> metadata = new HashMap<>();
-        metadata.put("x-amz-meta-myVal", "test");
-        PutObjectRequest objectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(fileObjectKeyName)
-                .metadata(metadata)
-                .build();
-        System.out.println("stream available is "+targetStream.available());
-//        System.out.println("stream size is "+getInputStreamLength(targetStream));
-        s3.putObject(objectRequest, RequestBody.fromInputStream(targetStream,targetStream.available()));
+        metadata.put("x-ms-meta-myVal", "test");
 
+        BlobHttpHeaders headers = new BlobHttpHeaders().setContentType("text/plain");
+        BlobParallelUploadOptions options = new BlobParallelUploadOptions(targetStream)
+                .setHeaders(headers)
+                .setMetadata(metadata);
+
+        System.out.println("stream available is " + targetStream.available());
+        blobClient.uploadWithResponse(options, null, null);
     }
+
     public static void uploadImage(String fileObjectKeyName, InputStream stream) throws IOException {
-        String bucketName = "testbucket3rag";
-        Region region = Region.EU_NORTH_1;
-        S3Client s3 = S3Client.builder()
-                .region(region)
-                .build();
+        String containerName = "testbucket3rag";
+        String endpoint = "https://yourstorageaccount.blob.core.windows.net";
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .endpoint(endpoint)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+        BlobClient blobClient = containerClient.getBlobClient(fileObjectKeyName);
 
+        BlobHttpHeaders headers = new BlobHttpHeaders().setContentType("image/jpeg");
+        BlobParallelUploadOptions options = new BlobParallelUploadOptions(stream)
+                .setHeaders(headers);
 
-        PutObjectRequest objectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(fileObjectKeyName)
-                .build();
-        s3.putObject(objectRequest, RequestBody.fromInputStream(stream,getInputStreamLength(stream)));
+        blobClient.uploadWithResponse(options, null, null);
     }
+
     public static int getInputStreamLength(InputStream inputStream) throws IOException {
         byte[] buffer = new byte[1024];
         int chunkBytesRead = 0;
         int length = 0;
-        while((chunkBytesRead = inputStream.read(buffer)) != -1) {
+        while ((chunkBytesRead = inputStream.read(buffer)) != -1) {
             length += chunkBytesRead;
         }
         return length;
     }
+
     private static ByteBuffer getRandomByteBuffer(int size) throws IOException {
         byte[] b = new byte[size];
         new Random().nextBytes(b);
